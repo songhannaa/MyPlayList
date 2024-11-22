f4d1b089d0930fa69b1d0082096021dd7d97a015
songhannaa <shn0322@kakao.com>
2024-11-23 08:57:05 +0900

    레이아웃 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div>
       <div style={{ display: 'flex' }}>
-      <Sidebar />  {/* 사이드바 추가 */}
+      <Sidebar /> 
       <main style={{ marginLeft: '250px', padding: '20px', flex: 1 }}>
         {children} 
       </main>
